cf9a23927d1c83212783e2cafe592066e2ed9efa
anji12345678 <114460376+anji12345678@users.noreply.github.com>
2022-09-27 09:47:04 +0530

    String _operations
    String example

---
+++ string_operations/slicing.py
@@ -0,0 +1,13 @@
+city="bangalore"
+print(city[3])
+print(city[-6])
+print(city[1:5])
+print(city[2:])
+print(city[-2:])
+
+"""output:::
+g
+g
+anga
+ngalore
+re"""
---
+++ string_operations/string-exp.py
@@ -0,0 +1,13 @@
+city="bangalore is a nice city"
+print(city.upper())
+print(city.capitalize())
+print(city.title())
+
+"""output:::
+BANGALORE IS A NICE CITY
+Bangalore is a nice city
+Bangalore Is A Nice City"""
+
+print(dir(city))
+
+print(city.isalpha())
